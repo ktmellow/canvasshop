9a11a0e2ab330b8799a6e0edbc4d2b5e0ae8d67b
Katherine <thrynn@Katherines-MBP.attlocal.net>
2018-06-12 22:13:49 -0700

    gave state to parent of Tools so Canvas may know selectedTool

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Canvas from './components/canvas/canvas';
 import Tools from './components/tools';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    
+    this.state = {
+      selectedTool: ""
+    };
+    this.handleToolClick = this.handleToolClick.bind(this);
+  }
+
+  handleToolClick(toolBtn) {
+    const newState = this.state;
+    newState.selectedTool = toolBtn;
+    this.setState(Object.assign({}, newState));
+  }
+
   render() {
     return (
       <div className="App">
-        <Tools />
+        <Tools handleToolClick={this.handleToolClick} selectedTool={this.state.selectedTool} />
         <Canvas />
       </div>
     );
--- src/components/tools/index.jsx
+++ src/components/tools/index.jsx
@@ -5,29 +5,18 @@ class Tools extends Component {
   constructor() {
     super()
 
-    this.state = {
-      selectedTool: ""
-    };
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(toolBtn) {
-    const newState = this.state;
-    newState.selectedTool = toolBtn;
-    this.setState(Object.assign({}, newState));
-    console.log("handleClick", toolBtn, this.state)
   }
 
   isActive(tool) {
-    return this.state.selectedTool === tool ? "selected" : "";
+    return this.props.selectedTool === tool ? "selected" : "";
   }
 
   render() {
+
     return (
       <div className="tools">
         <ul>
-          <li className={`tool ${this.isActive("line")}`} ref="line" onClick={this.handleClick.bind(this, "line")}>
+          <li className={`tool ${this.isActive("line")}`} ref="line" onClick={this.props.handleToolClick.bind(this, "line")}>
             line
           </li>
           <li className="tool">
